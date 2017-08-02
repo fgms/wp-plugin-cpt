6db053f9bb8f70dd771ef55cfaaa5721f5926232
sturple <shawn.turple@shaw.ca>
2017-08-02 09:28:17 -0700

    Updated  slick-carousel.php to switch allowed tags as all lower case and adding property attribute for the javascript object value which was camelcase version of allowed attributes for shortcode.

--- shortcodes/slick/slick-carousel.php
+++ shortcodes/slick/slick-carousel.php
@@ -2,15 +2,14 @@
 call_user_func(function () {
   add_shortcode('slick_carousel',function ($atts, $content) {
     $slick_path = get_stylesheet_directory(). '/../../../vendor/kenwheeler/slick/slick/';
-
     if (file_exists($slick_path.'slick.min.js') and file_exists($slick_path.'slick.css')){
       $atts['responsive'] = (empty($atts['responsive'])) ? '' : $atts['responsive'];
-      $atts['slidesToShow'] = (empty($atts['slidesToShow'])) ? '3' : $atts['slidesToShow'];
-      $atts['prevArrow'] = (empty($atts['prevArrow'])) ? false : $atts['prevArrow'];
-      $atts['nextArrow'] = (empty($atts['nextArrow'])) ? '' : $atts['nextArrow'];
+      $atts['prevarrow'] = (empty($atts['prevarrow'])) ? false : $atts['prevarrow'];
+      $atts['nextarrow'] = (empty($atts['nextarrow'])) ? '' : $atts['nextarrow'];
 
       $data = process_attributes($atts);
       load_assets($data);
+      //echo '<pre>'; print_R($atts);echo '</pre>';
       //echo '<pre>'; print_R($data);echo '</pre>';
       echo sprintf(
         '<div id="%1$s">%2$s</div>',
@@ -40,11 +39,9 @@ call_user_func(function () {
     }';
     if (file_exists($plugin_path.'assets/js/script.js')){
       $script = sprintf('
-        var options = %2$s;
-        /*console.log(options);*/
         jQuery(function($){
           if ((typeof $().slick === "function") && ( $("#%1$s").length > 0 )){
-            $("#%1$s").slick(options);
+            $("#%1$s").slick(%2$s);
           }
         });',
         $data['id'],
@@ -59,7 +56,8 @@ call_user_func(function () {
     $data = [];
     $allowed = get_allowed_options();
     foreach ($atts as $key=>$value){
-      $value = ((empty($value)) AND (!empty($allowed[$key]['default']))) ? (string)$allowed[$key]['default'] : $value;
+      $value = ((empty($value)) AND (!empty($allowed[$key]['default']))) ? $allowed[$key]['default'] : $value;
+      $value =  html_entity_decode($value, ENT_QUOTES, 'UTF-8');
       if (!empty($allowed[$key])){
         // checking type.
         if ($allowed[$key]['type'] === 'boolean'){
@@ -80,7 +78,7 @@ call_user_func(function () {
           }
         }
         // get default if empty, otherwise use value.
-        $data[$key] = $value ;
+        $data[$allowed[$key]['property']] = (string)$value ;
       }
     }
     return $data;
@@ -88,20 +86,20 @@ call_user_func(function () {
 
   function get_allowed_options(){
     return [
-      'id'              => [ 'type' => 'string', 'default' => 'slick-carousel-default'  ],
-      'accessibilty'    => ['type' => 'boolean', 'default' => true  ],
-      'autoplay'        => ['type' => 'boolean', 'default' => false ],
-      'autoplaySpeed'   => ['type' => 'integer', 'default' => 3000  ],
-      'infinite'        => ['type' => 'boolean', 'default' => false],
-      'responsive'      => [ 'type' => 'string', 'default' => '[{breakpoint: 1199, settings: { slidesToShow : 3}},{breakpoint: 989, settings : {slidesToShow : 2}},{breakpoint: 480, settings: {slidesToShow: 1}}]'  ],
-      'slidesPerRow'    => ['type' => 'integer', 'default' => 3  ],
-      'slidesToShow'    => ['type' => 'integer', 'default' => 3  ],
-      'slidesToScroll'  => ['type' => 'integer', 'default' => 1  ],
-      'speed'           => ['type' => 'integer', 'default' => 300  ],
-      'dots'            => ['type' => 'boolean', 'default' => true],
-      'arrows'          => ['type' => 'boolean', 'default' => true ],
-      'prevArrow'       => ['type' => 'html', 'default' =>'<a class="animiated-control-arrow control-arrow-prev "><span ></span></a>'],
-      'nextArrow'       => ['type' => 'html', 'default' =>'<a class="animiated-control-arrow control-arrow-next "><span ></span></a>']
+      'id'              => [ 'type' => 'string', 'property'=>'id', 'default' => 'slick-carousel-default'  ],
+      'accessibilty'    => ['type' => 'boolean', 'property'=>'accessibilty',  'default' => true  ],
+      'autoplay'        => ['type' => 'boolean', 'property'=>'autoplay',  'default' => false ],
+      'autoplayspeed'   => ['type' => 'integer', 'property'=>'autoplaySpeed',  'default' => 3000  ],
+      'infinite'        => ['type' => 'boolean', 'property'=>'infinite',  'default' => false],
+      'responsive'      => [ 'type' => 'string', 'property'=>'responsive',  'default' => '[{breakpoint: 1199, settings: { slidesToShow : 3}},{breakpoint: 989, settings : {slidesToShow : 2}},{breakpoint: 480, settings: {slidesToShow: 1}}]'  ],
+      'slidesperrow'    => ['type' => 'integer', 'property'=>'slidesPerRow',  'default' => 3  ],
+      'slidestoshow'    => ['type' => 'integer', 'property'=>'slidesToShow',  'default' => 3  ],
+      'slidestoscroll'  => ['type' => 'integer', 'property'=>'slidesToScroll',  'default' => 1  ],
+      'speed'           => ['type' => 'integer', 'property'=>'speed',  'default' => 300  ],
+      'dots'            => ['type' => 'boolean', 'property'=>'dots',  'default' => true],
+      'arrows'          => ['type' => 'boolean', 'property'=>'arrows',  'default' => true ],
+      'prevarrow'       => ['type' => 'html', 'property'=>'prevArrow',  'default' =>'<a class="animiated-control-arrow control-arrow-prev "><span ></span></a>'],
+      'nextarrow'       => ['type' => 'html', 'property'=>'nextArrow',  'default' =>'<a class="animiated-control-arrow control-arrow-next "><span ></span></a>']
     ];
   }
 });
